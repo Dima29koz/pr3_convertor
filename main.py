ac8656de0e4e6606b31be576e1bdae8daa94460a
Dmitry Kozerovsky <dima29koz@yandex.ru>
2021-09-28 17:28:30 +0300

    added image object getter

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import os
+from typing import Optional
+
+from PIL import Image, ImageFile, UnidentifiedImageError
 
 
 def get_file_names(path: str):
     return os.listdir(path)
 
 
+def get_image_obj(file_name) -> Optional[ImageFile.ImageFile]:
+    try:
+        image = Image.open(file_name)
+    except FileNotFoundError:
+        print('file not found')
+        return
+    except ValueError:
+        print('file cant be read')
+        return
+    except UnidentifiedImageError:
+        print('image cannot be opened and identified')
+        return
+    except TypeError:
+        print('type error')
+        return
+    else:
+        return image
+
+
 def main():
     path = 'inputdata'
     files = get_file_names(path)
+    images = [get_image_obj(f'{path + "/" + file}') for file in files]
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 
-from main import get_file_names
+from PIL import ImageFile
+from main import get_file_names, get_image_obj
 
 
 class CalculateCase(unittest.TestCase):
@@ -8,3 +9,7 @@ class CalculateCase(unittest.TestCase):
     def test_get_files(self):
         files = get_file_names('inputdata')
         self.assertEqual(files, ['1.png', '2.bmp', '3.jpg', '4.gif', '5.tif'])
+
+    def test_get_image(self):
+        image = get_image_obj('inputdata/1.png')
+        self.assertTrue(isinstance(image, ImageFile.ImageFile) or image is None)
