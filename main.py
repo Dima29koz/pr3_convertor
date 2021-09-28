b9eb4238d80d39ffb6e0ddb6ea1fc1c2da689b66
Dmitry Kozerovsky <dima29koz@yandex.ru>
2021-09-28 18:39:57 +0300

    refactored: now based on class ImageConverter

--- main.py
+++ main.py
@@ -4,58 +4,67 @@ from typing import Optional
 from PIL import Image, ImageFile, UnidentifiedImageError
 
 
-def get_file_names(path: str):
-    return os.listdir(path)
+class ImageConverter:
+    def __init__(self, path: str = 'inputdata'):
+        self.path = path
 
+    def convert(self, path=''):
+        if path:
+            self.path = path
 
-def get_image_obj(file_name) -> Optional[ImageFile.ImageFile]:
-    try:
-        image = Image.open(file_name)
-    except FileNotFoundError:
-        print('file not found')
-        return
-    except ValueError:
-        print('file cant be read')
-        return
-    except UnidentifiedImageError:
-        print('image cannot be opened and identified')
-        return
-    except TypeError:
-        print('type error')
-        return
-    else:
-        return image
+        files = self.get_file_names()
+        for file in files:
+            image = self.get_image_obj(f'{self.path + "/" + file}')
+            if image:
+                file_name, ext = os.path.splitext(file)
+                extension = str(input(f'Укажите конечное расширение для файла {file}: '))
+                self.save_with_extension(image, file_name, extension if extension else 'jpg')
+                # for ex in ['jpg', 'png', 'tif', 'gif', 'bmp']:
+                #     save_with_extension(image, file_name, ex)
 
+    def get_file_names(self):
+        return os.listdir(self.path)
 
-def save_with_extension(image: ImageFile.ImageFile, file_name: str, extension: str):
-    if extension not in ['jpg', 'png', 'tif', 'gif', 'bmp']:
-        print("Данное расширение не поддерживается")
-        return
-    rgb_im = image.convert('RGB')
-    try:
-        rgb_im.save(f'out/{file_name}.{extension}')
-    except TypeError:
-        rgb_im.save(f'tmp/{file_name}.jpg')
-        img = get_image_obj(f'tmp/{file_name}.jpg')
-        print(f'[SAVED TMP] {img.filename} -> {file_name}.jpg')
+    @staticmethod
+    def get_image_obj(file_name) -> Optional[ImageFile.ImageFile]:
+        try:
+            image = Image.open(file_name)
+        except FileNotFoundError:
+            print('file not found')
+            return
+        except ValueError:
+            print('file cant be read')
+            return
+        except UnidentifiedImageError:
+            print('image cannot be opened and identified')
+            return
+        except TypeError:
+            print('type error')
+            return
+        else:
+            return image
 
-        save_with_extension(img, file_name, extension)
-    else:
-        print(f'[SAVED] {image.filename} -> {file_name}.{extension}')
-        return True
+    def save_with_extension(self, image: ImageFile.ImageFile, file_name: str, extension: str):
+        if extension not in ['jpg', 'png', 'tif', 'gif', 'bmp']:
+            print("Данное расширение не поддерживается")
+            return
+        rgb_im = image.convert('RGB')
+        try:
+            rgb_im.save(f'out/{file_name}.{extension}')
+        except TypeError:
+            rgb_im.save(f'tmp/{file_name}.jpg')
+            img = self.get_image_obj(f'tmp/{file_name}.jpg')
+            print(f'[SAVED TMP] {img.filename} -> {file_name}.jpg')
+
+            self.save_with_extension(img, file_name, extension)
+        else:
+            print(f'[SAVED] {image.filename} -> {file_name}.{extension}')
+            return True
 
 
 def main():
-    path = 'inputdata'
-    files = get_file_names(path)
-    for file in files:
-        image = get_image_obj(f'{path + "/" + file}')
-        if image:
-            file_name, ext = os.path.splitext(file)
-            extension = str(input(f'Укажите конечное расширение для файла {file}: '))
-            save_with_extension(image, file_name, extension if extension else 'jpg')
-            # for ex in ['jpg', 'png', 'tif', 'gif', 'bmp']:
-            #     save_with_extension(image, file_name, ex)
+    ic = ImageConverter()
+    ic.convert()
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,19 +1,22 @@
 import unittest
 
 from PIL import ImageFile
-from main import get_file_names, get_image_obj, save_with_extension
+from main import ImageConverter
 
 
 class CalculateCase(unittest.TestCase):
 
     def test_get_files(self):
-        files = get_file_names('inputdata')
+        ic = ImageConverter()
+        files = ic.get_file_names()
         self.assertEqual(files, ['1.png', '2.bmp', '3.jpg', '4.gif', '5.tif'])
 
     def test_get_image(self):
-        image = get_image_obj('inputdata/1.png')
+        ic = ImageConverter()
+        image = ic.get_image_obj(f'{ic.path}/1.png')
         self.assertTrue(isinstance(image, ImageFile.ImageFile) or image is None)
 
     def test_save_image(self):
-        image = get_image_obj('inputdata/1.png')
-        self.assertTrue(save_with_extension(image, 'test', 'jpg'))
+        ic = ImageConverter()
+        image = ic.get_image_obj(f'{ic.path}/1.png')
+        self.assertTrue(ic.save_with_extension(image, 'test', 'jpg'))
